fead2b341c6be07268da3a3544ea5d9777c94324
shinhee-lee <oky0706@gmail.com>
2024-04-04 15:54:37 +0900

    M3: Login 시 header style 지정

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { Logo, Nav } from "./styles";
+import { Login, LoginContainer, Logo, Nav } from "./styles";
 
 type Props = {};
 
 function Header() {
+  const renderHeader = () => {
+    //로그인 되면 로그인 헤더 보여주기
+    return (
+      <LoginContainer>
+        <Login>Login</Login>
+      </LoginContainer>
+    );
+  };
   return (
     <Nav>
       <Logo
         src="https://cnbl-cdn.bamgrid.com/assets/7ecc8bcb60ad77193058d63e321bd21cbac2fc67281dbd9927676ea4a4c83594/original"
         alt="Disney Logo"
       />
+
+      {renderHeader()}
     </Nav>
   );
 }
