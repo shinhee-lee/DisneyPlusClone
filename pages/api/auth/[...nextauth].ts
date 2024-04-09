53d92be3927c8ecd2e75711d34d1d091e1844cf2
shinhee-lee <oky0706@gmail.com>
2024-04-09 16:47:52 +0900

    M5: /login redirect

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,20 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+  secret: process.env.SECRET,
+  callbacks: {
+    async redirect({ url }) {
+      // callback url 허용
+      if (url.includes("/login")) return "/";
+      if (!url.includes("/")) return "/login";
+      return url;
+    },
+  },
+});
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,28 @@
 import SignUp from "@/components/SignUp";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Login() {
-  return (
-    <>
-      <SignUp />
-    </>
-  );
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (session) {
+      //홈으로 리디렉트
+      router.push({
+        pathname: "/",
+      });
+    }
+    return () => {};
+  }, []);
+
+  if (status === "loading") return <p>Loading</p>;
+
+  if (!session)
+    return (
+      <>
+        <SignUp />
+      </>
+    );
 }
